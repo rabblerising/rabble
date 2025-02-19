f530b61e9ba843f76808138e9a132af734e3bc0b
rabblerising <rabblerouserrising@gmail.com>
2025-02-19 15:28:45 -0500

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>Rabble</title>
+  <base href="https://rabblerising.github.io/rabble/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-XD2GP3DN.js" type="module"></script></body>
+</html>
+`;
